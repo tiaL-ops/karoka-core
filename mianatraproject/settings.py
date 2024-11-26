3c0103d8a274f392fb2d29c453d058fb3d4edcda
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2024-11-25 22:03:56 -0500

    please

--- mianatraproject/settings.py
+++ mianatraproject/settings.py
@@ -153,6 +153,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Add global static directory if needed
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
