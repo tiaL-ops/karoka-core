cb0f18cb576bae8e34ddf6a1f69738c07c256ee4
Landy Rakotoarison <landytiaa@gmail.com>
2025-06-10 00:38:33 -0700

    Tireeeeed

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,19 +1,23 @@
-# db/database.py
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+# Only load .env in non-production
 if os.getenv("ENVIRONMENT") != "production":
     load_dotenv()
 
+# Read the single URL var
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Fall back to “development” if ENVIRONMENT isn’t set
 env = os.getenv("ENVIRONMENT", "development")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-print(f"ENVIRONMENT 🙂: {env}")
+
+print(f"ENVIRONMENT 🥳: {env}")
 print(f"Using DB: {DATABASE_URL}")
 
 Base = declarative_base()
